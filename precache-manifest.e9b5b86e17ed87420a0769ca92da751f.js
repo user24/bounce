6b8898e6accdf6230f79aec4940e9fb6abf97213
Howard Yeend <howard.yeend@gmail.com>
2020-05-10 17:03:08 +0100

    Updates

---
+++ precache-manifest.e9b5b86e17ed87420a0769ca92da751f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644060c20af2417aaab6335d4e9d858e",
+    "url": "/bounce/index.html"
+  },
+  {
+    "revision": "4c3a0090af2ac71e1e45",
+    "url": "/bounce/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fd5bc1e96549d1578d79",
+    "url": "/bounce/static/js/2.ad86b3ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bounce/static/js/2.ad86b3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3a0090af2ac71e1e45",
+    "url": "/bounce/static/js/main.10340028.chunk.js"
+  },
+  {
+    "revision": "543611f519b4780619f2",
+    "url": "/bounce/static/js/runtime-main.d8c51edb.js"
+  }
+]);
